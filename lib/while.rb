7cd4a0fbeaed427fc87594b3eba02ab8dd58e0dc
Anthony Catalfo <anthonycatalfo@gmail.com>
2017-12-12 05:42:16 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -2,7 +2,7 @@ def using_while
 	levitation_force = 6
 	while conditional [do]
    puts "Wingardium Leviosa"
-	 levitation_force += 1
+	 levitation_force = levitation_force + 1
 	end
 	#your code here
 end
